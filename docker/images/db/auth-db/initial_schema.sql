5f8d392b6aa35d78049769fb04b79e4b6e6280b6
lolinatorishere <78425011+Lolinatorishere@users.noreply.github.com>
2024-07-24 11:54:56 +0100

    Altered the dockerfiles to allow for independant container setups as the previous way was inandequate for the project
     Changes to be committed:
    	modified:   docker-compose.yml
    	renamed:    docker/images/db/Dockerfile -> docker/images/db/auth-db/Dockerfile
    	renamed:    docker/images/db/initial_schema.sql -> docker/images/db/auth-db/initial_schema.sql
    	new file:   docker/images/db/bl-db/Dockerfile
    	new file:   docker/images/db/bl-db/initial_schema.sql
    	renamed:    docker/images/javascript/Dockerfile -> docker/images/javascript/auth-api/Dockerfile
    	new file:   docker/images/javascript/bl-api/Dockerfile
    	new file:   docker/images/javascript/frontend/Dockerfile
    	new file:   docker/images/javascript/importer/Dockerfile

--- docker/images/db/auth-db/initial_schema.sql
+++ docker/images/db/auth-db/initial_schema.sql
@@ -1,14 +1,6 @@
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-/* Sample table and data that we can insert once the database is created for the first time */
-CREATE TABLE public.teachers (
-	name    VARCHAR (100),
-	city    VARCHAR(100),
-	created_on      TIMESTAMP NOT NULL DEFAULT NOW(),
-	updated_on      TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
 /* Default users table */
 CREATE TABLE public.users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -16,6 +8,7 @@ CREATE TABLE public.users (
     password CHAR(64)
 );
 
+/* Default user permissions table */
 CREATE TABLE public.user_perms (
     user_uuid UUID NOT NULL UNIQUE,
     read TINYINT(1),
@@ -27,5 +20,3 @@ CREATE TABLE public.user_perms (
 /*default user for the auth api*/
 INSERT INTO users(username, password) VALUES('root', encode(digest('root', 'sha256'), 'hex'));
 
-INSERT INTO teachers(name, city) VALUES('Luís Teófilo', 'Porto');
-INSERT INTO teachers(name, city) VALUES('Ricardo Castro', 'Braga');
